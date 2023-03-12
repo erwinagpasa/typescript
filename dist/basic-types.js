396a8943f2cedeed97ca43a68f0f03c83027956b
erwinagpasa <rootaccess000@gmail.com>
2023-03-12 22:19:55 +0300

    basic types

---
+++ dist/basic-types.js
@@ -0,0 +1,9 @@
+"use strict";
+//Basic Types
+let id = 5;
+let developer = "Erwin Agpasa";
+let isPublished = true;
+let anything = "Any value";
+//You can also initialize 
+let age;
+age = 40;
---
+++ src/basic-types.ts
@@ -0,0 +1,12 @@
+//Basic Types
+let id: number = 5
+
+let developer: string = "Erwin Agpasa"
+
+let isPublished: boolean = true
+
+let anything: any = "Any value"
+
+//You can also initialize 
+let age: number
+age = 40
